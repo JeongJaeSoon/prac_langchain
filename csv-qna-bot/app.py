d39ab7828d1f32180052d44e6a0c1790595ea28c
JeongJaeSoon <jeongjaesoon@lifull.com>
2023-09-01 10:45:22 +0900

    feat: slack chat update

--- csv-qna-bot/app.py
+++ csv-qna-bot/app.py
@@ -1,8 +1,10 @@
 import logging
 import os
+import traceback
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from langchain.callbacks.base import AsyncCallbackHandler
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders.csv_loader import CSVLoader
@@ -22,51 +24,91 @@ slack_app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
 socket_handler = AsyncSocketModeHandler(slack_app, os.environ["SLACK_APP_TOKEN"])
 
 loader = CSVLoader(file_path="sample.csv", encoding="utf-8", csv_args={'delimiter': ','})
-data = loader.load()
-embeddings = OpenAIEmbeddings()
-vector_store = FAISS.from_documents(data, embeddings)
+documents = loader.load()
 
-chain = ConversationalRetrievalChain.from_llm(
-  llm = ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo'),
-  retriever=vector_store.as_retriever()
-)
+embeddings = OpenAIEmbeddings()
+
+vectorstore = FAISS.from_documents(documents, embeddings)
+retriever = vectorstore.as_retriever()
 
 bot_ids = os.environ["SLACK_BOT_IDS"].split(",")
 whitelist_channels = os.environ["SLACK_WHITELIST_CHANNELS"].split(",")
 
 @slack_app.event("app_mention")
 async def handle_events(event, client, say):
-    contents = event['blocks'][0]['elements'][0]['elements']
+    progress = await say(f":sun-spin:", thread_ts=event['ts'])
 
-    bot_id = next((item["user_id"] for item in contents if item.get("user_id") in bot_ids), None)
-    user_id = event['user']
+    try:
+        contents = event['blocks'][0]['elements'][0]['elements']
 
-    text = event['text']
-    question:str = text.split(f'<@{bot_id}>')[-1].strip()
+        bot_id = next((item["user_id"] for item in contents if item.get("user_id") in bot_ids), None)
+        user_id = event['user']
 
-    if not question:
-        await say(f"おいおい、内容がないぞ <@{user_id}> さん。", thread_ts=event['ts'])
+        text = event['text']
+        question:str = text.split(f'<@{bot_id}>')[-1].strip()
+
+        if not question:
+            await client.chat_update(
+                channel=progress['channel'],
+                ts=progress['ts'],
+                text=f"おいおい、内容がないぞ <@{user_id}> さん。"
+            )
+            return
+
+        if event['channel'] not in whitelist_channels:
+            await client.chat_update(
+                channel=progress['channel'],
+                ts=progress['ts'],
+                text=f"すみません、<@{bot_id}> は特定のチャンネルでのみ動作します。"
+            )
+            return
+
+        llm = ChatOpenAI(
+            # FIXME: Streaming is not working yet with the ConversationalRetrievalChain
+            # streaming=True,
+            # callbacks=[
+            #     SlackUpdateAsyncHandler(
+            #         client=client,
+            #         channel=progress['channel'],
+            #         ts=progress['ts']
+            #     )
+            # ],
+            temperature=0,
+            model_name='gpt-3.5-turbo',
+        )
+
+        chain = ConversationalRetrievalChain.from_llm(
+            llm = llm,
+            retriever=retriever
+        )
+
+        chat_history = []
+
+        print("start")
+        result = chain({
+            "question": question,
+            "chat_history": chat_history
+        })
+        answer = result['answer']
+
+        await client.chat_update(
+            channel=progress['channel'],
+            ts=progress['ts'],
+            text=f"{answer}"
+        )
+        chat_history.append((question, answer))
+
+        print(result)
         return
 
-    if event['channel'] not in whitelist_channels:
-        await say(f"すみません、<@{bot_id}> は特定のチャンネルでのみ動作します。", thread_ts=event['ts'])
+    except Exception as e:
+        err_msg = traceback.format_exc()
+        await client.chat_update(
+            channel=progress['channel'],
+            ts=progress['ts'],
+            text=f"エラーが発生しました。下記のエラーメッセージを担当開発者に共有してください。\n```{err_msg}```",
+        )
         return
-
-    await say(f":sun-spin:", thread_ts=event['ts'])
-
-    chat_history = []
-
-    print("start")
-    result = chain({
-      "question": question,
-      "chat_history": chat_history
-    })
-    answer = result["answer"]
-    print("end")
-
-    chat_history.append((question, answer))
-
-    await say(f"Q: {question}\n\nA: {answer}", thread_ts=event['ts'])
 
 @fastapi_app.get("/health")
 async def health():
@@ -75,3 +117,18 @@ async def health():
 @fastapi_app.on_event("startup")
 async def startup():
     await socket_handler.connect_async()
+
+# class SlackUpdateAsyncHandler(AsyncCallbackHandler):
+#     def __init__(self, client, channel, ts, initial_context="") -> None:
+#         self.client = client
+#         self.channel = channel
+#         self.ts = ts
+#         self.context = initial_context
+
+#     async def on_llm_new_token(self, token: str, **kwargs) -> None:
+#         self.context += token
+#         await self.client.chat_update(
+#             channel=self.channel,
+#             ts=self.ts,
+#             text=f"{self.context}"
+#         )
